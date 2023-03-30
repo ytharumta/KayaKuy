7c45ce9f87a6ec566ea60664013201743881499f
yosafat-hashmicro <64425747+yosafat-hashmicro@users.noreply.github.com>
2023-03-30 22:28:26 +0700

    env

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 		fmt.Println("success read file environment")
 	}
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"))
 	DB, err = sql.Open("postgres", psqlInfo)
 	err = DB.Ping()
 	if err != nil {
